261e365e37a28b010e68c8d4ee9ba4827d1b572b
bohdan <bohdanstep23@gmail.com>
2023-03-30 22:15:00 +0300

    done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,19 @@
+meters = float(input("Введіть кількість метрів: "))
 
+print("1 - Метри в милі")
+print("2 - Метри в дюйми")
+print("3 - Метри в ярди")
+
+choice = input("Ваш вибір: ")
+
+if choice == "1":
+    mil = meters / 1609.344
+    print(meters, "метрів =", mil, "миль")
+elif choice == "2":
+    inches = meters * 39.37
+    print(meters, "метрів =", inches, "дюймів")
+elif choice == "3":
+    yards = meters * 1.0936
+    print(meters, "метрів =", yards, "ярдів")
+else:
+    print("Неправильно введено число, спробуйте iншу комбiнацiю.")
