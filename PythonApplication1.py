6d83abd6c27bdb4ab53cd93a9de32c35be5d0d86
bohdan <bohdanstep23@gmail.com>
2023-03-30 22:42:55 +0300

    done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,19 @@
+num1 = int(input("Введіть перше число: "))
+num2 = int(input("Введіть друге число: "))
+operation = input("Виберіть операцію (+, -, *, /): ")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+else:
+    print("Невірна операція!")
+    result = None
+
+if result is not None:
+    print("Результат операції:", result)
 
